1979275f16536b4ff81bfd605d194347dc935ad2
CJ Harries <cj@wizardsoftheweb.pro>
2018-06-02 17:02:52 -0500

    Test apply_dict_to_self

--- tests/components/test_config_object.py
+++ tests/components/test_config_object.py
@@ -26,5 +26,33 @@ class ConfigTestCase(TestCase):
         self.addCleanup(mkdirp_patcher.stop)
         set_patcher = patch.object(Config, 'set_with_defaults')
         self.mock_set = set_patcher.start()
+        config_dir_patcher = patch.object(Config, 'config_dir')
+        self.mock_config_dir = config_dir_patcher.start()
         self.config = Config()
         set_patcher.stop()
+        config_dir_patcher.stop()
+
+
+class ConstructorUnitTests(ConfigTestCase):
+
+    def test_calls(self):
+        self.mock_set.assert_called_once()
+        self.mock_mkdirp.assert_called_once()
+
+
+class ApplyDictToSelfUnitTests(ConfigTestCase):
+    FINAL = {
+        'one': 'two',
+        'three': 'four',
+    }
+    STARTING = {
+        'one': 'five',
+        'three': 'six',
+    }
+
+    def test_with_dict(self):
+        for key, value in list(self.STARTING.items()):
+            self.config[key] = value
+        self.assertNotEqual(self.config, self.FINAL)
+        self.config.apply_dict_to_self(self.FINAL)
+        self.assertEqual(self.config, self.FINAL)
