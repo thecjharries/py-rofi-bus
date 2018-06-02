6d276a6304f6c464b6a87b7f6ac6060ef658012d
CJ Harries <cj@wizardsoftheweb.pro>
2018-06-02 15:21:01 -0500

    Test app ctor

---
+++ tests/cli/test_application_cli.py
@@ -0,0 +1,42 @@
+# pylint: disable=W,C,R
+
+from __future__ import print_function
+
+from collections import OrderedDict
+from errno import EEXIST
+from unittest import TestCase
+
+from mock import call, MagicMock, patch
+
+from py_rofi_bus.cli import Application
+
+
+class ApplicationTestCase(TestCase):
+
+    def setUp(self):
+        self.construct_app()
+        self.addCleanup(self.wipe_app)
+
+    def wipe_app(self):
+        del self.app
+
+    def construct_app(self):
+        parser_patcher = patch('py_rofi_bus.cli.application.ArgumentParser')
+        self.mock_parser = parser_patcher.start()
+        self.addCleanup(parser_patcher.stop)
+        self.app = Application()
+
+
+class ConstructorUnitTests(ApplicationTestCase):
+
+    def test_vanilla(self):
+        self.mock_parser.assert_called_once()
+
+    def test_parent_parser(self):
+        PARSER = MagicMock()
+        PARENT = MagicMock(
+            add_parser=PARSER,
+        )
+        PARSER.assert_not_called()
+        app = Application(parent_parser=PARENT)
+        PARSER.assert_called_once()
