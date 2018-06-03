e3eb65530f11101e473e6444c378378533f0b770
CJ Harries <cj@wizardsoftheweb.pro>
2018-06-03 06:45:34 -0500

    Pass arguments down to Config (whoops)

--- py_rofi_bus/components/mixins/has_config.py
+++ py_rofi_bus/components/mixins/has_config.py
@@ -6,6 +6,6 @@ class HasConfig(object):
 
     def __init__(self, config=None, *args,  **kwargs):
         if config is None:
-            self.config = Config()
+            self.config = Config(*args,  **kwargs)
         else:
             self.config = config
